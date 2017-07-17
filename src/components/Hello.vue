5e0f4454841c49e723b4034d49999736cef0f30e
Stew Heckenberg <stewart.heckenberg@gmail.com>
2017-07-18 04:19:28 +1000

    buefy

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,53 +1,55 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-      <br>
-      <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+    <b-table
+      :data="tableData">
+
+      <template scope="props">
+
+          <b-table-column field="name" label="Name" sortable>
+              {{ props.row.name }}
+          </b-table-column>
+
+          <b-table-column field="description" label="Description">
+              {{ props.row.description }}
+          </b-table-column>
+
+          <b-table-column field="pax" label="Passengers" sortable>
+              {{ props.row.pax }}
+          </b-table-column>
+
+          <b-table-column field="pickup" label="Pickup Location" sortable>
+              {{ props.row.pickup }}
+          </b-table-column>
+
+          <b-table-column field="dropoff" label="Dropoff Location" sortable>
+              {{ props.row.dropoff }}
+          </b-table-column>
+
+          <b-table-column field="bookings" label="Bookings" sortable>
+            <span v-for="booking in props.row.bookings" class="tag is-success">
+                {{ new Date(booking.date).toLocaleDateString() }}
+            </span>
+          </b-table-column>
+
+      </template>
+
+      <div slot="empty" class="has-text-centered">
+          This table is empty!
+      </div>
+
+    </b-table>
   </div>
 </template>
 
 <script>
+import data from '../data.json'
+
 export default {
   name: 'hello',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      tableData: data
     }
   }
 }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
-</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,10 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
+import Buefy from 'buefy'
+import 'buefy/lib/buefy.css'
 
+Vue.use(Buefy)
 Vue.use(Router)
 
 export default new Router({
